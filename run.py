55a8b31c937a98c16b0f3d882de721b33ad74f8d
FanZy0127 <fanzy28@hotmail.fr>
2022-03-29 16:33:54 +0200

    Initiation des routes.

--- run.py
+++ run.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template
 from PyCalculotron.components.calculation.fibonacci import fibonacci
+from PyCalculotron.components.calculation.bernouilli import experience as exp, print_bernouilli_result_array as bern
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello world !"
+
 
 @app.route('/calculation/<operation>/<params>')
 def calculation(operation, params):
     if operation == "fibonacci":
         assert params > 0
         result = fibonacci(params)
+    elif operation == "bernouilli":
+        assert "r" in params and "n" in params and params.r > 0 and params.n > 0
+        experience = exp(params.n)
+        bernouilli_array = bern(params.r, params.n)
+        result = {"experience": experience, "bernouilli_array": bernouilli_array}
 
     return render_template("calculation.html", data={
-        "result":result
+        "result": result
     })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,7 +1,9 @@
 from PyCalculotron.run import index, calculation
 
+
 def test_index():
     assert index() == "Hello world !"
+
 
 def test_calculation():
     assert calculation("fibonacci", 9) == {"result":34}
