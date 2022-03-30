6f57a84baf03c3b0db6257fbfcec22d080e054c5
FanZy0127 <fanzy28@hotmail.fr>
2022-03-30 10:19:06 +0200

    changement de nomenclature

--- tests/test_components/test_calculation/test_bernouilli.py
+++ tests/test_components/test_calculation/test_bernouilli.py
@@ -5,5 +5,5 @@ def test_experience():
     assert 20 >= experience(20) > 0
 
 
-def test_print_bernouilli_result_array():
+def test_bernouilli_result_array():
     assert isinstance(bernouilli_result_array(30, 10), list) and len(bernouilli_result_array(30, 10)) == 30
